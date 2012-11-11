21a9c9befad41db0b34d971503d9b58d0b920bfb
James <james@opensourceopportunities.com>
2012-11-10 18:02:59 -0800

    First commit

---
+++ ihavebadges.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name           IHaveBadges 
+// @namespace      stackoverflow
+// @author         jmort253 (http://stackoverflow.com/users/552792)
+// @description    Link the badges in the toolbar to the badge section 
+// @include        http://*.stackoverflow.com/*
+// @include        http://*.serverfault.com/*
+// @include        http://serverfault.com/*
+// @include        http://*.superuser.com/*
+// @include        http://superuser.com/*
+// @include        http://*.stackexchange.com/*
+// @include        http://mathoverflow.net/*
+// @include        http://stackapps.com/*
+// ==/UserScript==
+
+function with_jquery(f) {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.textContent = "(" + f.toString() + ")(jQuery)";
+    document.body.appendChild(script);
+};
+
+with_jquery(function($) {
+  
+    var badge_anchor = $('<a id="badge_anchor" href="#"></a>'); 
+    $('#hlinks-user > [title*="badge"]').each(function() {
+        console.info($(this).html());
+        badge_anchor.append($(this));
+    });
+ 
+    var profileLinkArr = $('#hlinks-user > .profile-link').get(0).toString().split('/');
+
+    badge_anchor.attr("href","/users/" + profileLinkArr[4] + "/" + profileLinkArr[5] + "?tab=badges");
+
+//    var reputationElem = $('#hlinks-user > a[href*="reputation"]');
+    var reputationElem;
+    var reputation_score = $('#hlinks-user .reputation-score');
+    if(reputation_score.parent().attr("href") != undefined && reputation_score.parent().attr("href").contains("reputation")) {
+        reputationElem = reputation_score.parent();
+    } else { 
+        reputationElem = reputation_score;
+    }
+
+    reputationElem.after(badge_anchor);
+ 
+});
